5ca1908d8cccba0d5a98469ec0543cf661217254
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-08-04 12:23:44 -0400

    refac

---
+++ CndSemantics/Definitions.lean
@@ -0,0 +1,125 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Real.Basic
+import Std.Data.HashMap
+
+
+namespace CnD
+
+structure Rect where
+  xmin : Rat
+  ymin : Rat
+  xmax : Rat
+  ymax : Rat
+
+
+structure Box where
+  xmin : Rat
+  ymin : Rat
+  width : Rat
+  height : Rat
+deriving BEq
+
+
+
+instance : Inhabited Box where
+  default := { xmin := 0.0, ymin := 0.0, width := 1.0, height := 1.0 }
+
+abbrev Atom := Nat
+abbrev Realization := Atom → Option Box
+
+
+
+def horizontally_aligned (a b : Box) : Bool := a.ymin = b.ymin
+def vertically_aligned (a b : Box) : Bool := a.xmin  = b.xmin
+
+def left_of (a b : Box) : Bool := a.xmin + a.width < b.xmin
+-- def right_of (a b : Box) : Bool := a.xmin + a.width > b.xmin
+def above (a b : Box) : Bool := a.ymin + a.height < b.ymin
+-- def below (a b : Box) : Bool := (a.ymin + a.height) > b.ymin
+
+-- def directly_left (a b : Box) : Bool := a.xmin + a.width = b.xmin ∧ horizontally_aligned a b
+-- def directly_right (a b : Box) : Bool := b.xmin + b.width = a.xmin ∧ horizontally_aligned a b
+-- def directly_above (a b : Box) : Bool := a.ymin + a.height = b.ymin ∧ vertically_aligned a b
+-- def directly_below (a b : Box) : Bool := b.ymin + b.height = a.ymin ∧ vertically_aligned a b
+
+
+
+-- TODO: Cyclic constraints.
+
+
+
+
+
+-- NOW WE NEED TO WRITE THE ATOM VERSION OF THESE FUNCTIONS (IS THIS OVERKILL?)
+def atom_left_of (a b : Atom) (realization : Realization) : Bool :=
+  match realization a, realization b with
+  | some boxA, some boxB => left_of boxA boxB
+  | _, _ => false
+
+
+def atom_above (a b : Atom) (realization : Realization) : Bool :=
+  match realization a, realization b with
+  | some boxA, some boxB => above boxA boxB
+  | _, _ => false
+
+
+def atom_horizontally_aligned (a b : Atom) (realization : Realization) : Bool :=
+  match realization a, realization b with
+  | some boxA, some boxB => horizontally_aligned boxA boxB
+  | _, _ => false
+
+def atom_vertically_aligned (a b : Atom) (realization : Realization) : Bool :=
+  match realization a, realization b with
+  | some boxA, some boxB => vertically_aligned boxA boxB
+  | _, _ => false
+
+
+
+
+def contains (rect : Rect) (box : Box) : Bool :=
+  rect.xmin ≤ box.xmin &&
+  box.xmin + box.width ≤ rect.xmax &&
+  rect.ymin ≤ box.ymin &&
+  box.ymin + box.height ≤ rect.ymax
+
+-- I think we need to re write this.
+-- I think it is wrong?
+def grouped (boxes : List Box) (realization : Realization) : Prop :=
+  ∃ (rect : Rect),
+    -- Every box in the list is contained in the rectangle
+    (∀ box ∈ boxes, contains rect box) ∧
+    -- No box not in the list is contained in the rectangle
+    (∀ (n : Atom), match realization n with
+                   | some box => contains rect box → ∃ b ∈ boxes, b = box
+                   | none => True)
+
+
+
+def atoms_grouped (atoms : List Atom) (realization : Realization) : Prop :=
+  ∃ (rect : Rect),
+    -- Every box corresponding to the atoms is contained in the rectangle
+    (∀ atom ∈ atoms, match realization atom with
+                     | some box => contains rect box
+                     | none => False)
+      ∧
+    -- No box not corresponding to the atoms is contained in the rectangle
+    (∀ (n : Atom), match realization n with
+                   | some box => contains rect box → n ∈ atoms
+                   | none => True)
+
+
+
+-- Reduce this to the key (non-sugared constraints)
+inductive Constraint
+| left           (a b : Atom)
+| above          (a b : Atom)
+| horizontally_aligned (a b : Atom)
+| vertically_aligned (a b : Atom)
+| group          (S : Finset Atom) -- Maybe this should be finset?
+
+deriving BEq
+
+
+
+
+end CnD
--- CndSemantics/Semantics.lean
+++ CndSemantics/Semantics.lean
@@ -1,41 +1,38 @@
-import CndSemantics.Boxes
-import CndSemantics.Constraints
+import CndSemantics.Definitions
+
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Real.Basic
 
 namespace CnD
 
-def satisfies (R : Realization) : Constraint → Prop
-| Constraint.left a b => left_of (R a) (R b)
-| Constraint.right a b => right_of (R a) (R b)
-| Constraint.above a b => above (R a) (R b)
-| Constraint.below a b => below (R a) (R b)
-| Constraint.directly_left a b => directly_left (R a) (R b)
-| Constraint.directly_right a b => directly_right (R a) (R b)
-| Constraint.directly_above a b => directly_above (R a) (R b)
-| Constraint.directly_below a b => directly_below (R a) (R b)
+
+-- TODO: NO overlapping boxes in a Realization.
+
+
+-- TODO: THis needs to be re-written
+def satisfies (R : Realization) : Constraint → Prop :=
+| Constraint.left a b => atom_left_of (R a) (R b)
+| Constraint.above a b => atom_above (R a) (R b)
+| Constraint.horizontally_aligned a b => atom_horizontally_aligned (R a) (R b)
+| Constraint.vertically_aligned a b => atom_vertically_aligned (R a) (R b)
 | Constraint.group S => grouped (S.map R) R
 
 def well_typed (Γ : List Constraint) (R : Realization) : Prop :=
   ∀ C ∈ Γ, satisfies R C
 
+abbrev ConstraintSet := Finset Constraint
+def satisfies_all (R : Realization) (S : ConstraintSet) : Prop :=
+  ∀ c ∈ S, satisfies R c
 
-def decidable_satisfies (R : Realization) (C : Constraint) : Decidable (satisfies R C) :=
-  match C with
-  | Constraint.left a b =>
-    if h : left_of (R a) (R b) then isTrue h else isFalse h
-  | Constraint.right a b =>
-    if h : right_of (R a) (R b) then isTrue h else isFalse h
-  | Constraint.above a b =>
-    if h : above (R a) (R b) then isTrue h else isFalse h
-  | Constraint.below a b =>
-    if h : below (R a) (R b) then isTrue h else isFalse h
-  | Constraint.directly_left a b =>
-    if h : directly_left (R a) (R b) then isTrue h else isFalse h
-  | Constraint.directly_right a b =>
-    if h : directly_right (R a) (R b) then isTrue h else isFalse h
-  | Constraint.directly_above a b =>
-    if h : directly_above (R a) (R b) then isTrue h else isFalse h
-  | Constraint.directly_below a b =>
-    if h : directly_below (R a) (R b) then isTrue h else isFalse h
-  | Constraint.group S =>
-    if h : grouped (S.map R) R then isTrue h else isFalse h
+def models (S : ConstraintSet) : Set Realization :=
+  { R | satisfies_all R S }
+
+
+theorem refinement (S : ConstraintSet) (C : Constraint) :
+  models (S ∪ ({C} : Finset Constraint)) ⊆ models S := by
+  intro R h
+  unfold models satisfies_all at *
+  simp at *
+  exact And.left h
+
 end CnD
